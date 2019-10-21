e6ac6cfac3d6c96463bceecec57577c514204c1f
Alex S. Garzão <alexgarzao@gmail.com>
2019-10-20 21:28:56 -0300

    STV opcode implementation

---
+++ gogpt/usecases/opcodes/stv_opcode_test.go
@@ -0,0 +1,70 @@
+package opcodes
+
+import (
+	"testing"
+
+	"github.com/alexgarzao/gogpt-interpreter/gogpt/entities/constant_pool"
+	"github.com/alexgarzao/gogpt-interpreter/gogpt/entities/stack"
+	"github.com/alexgarzao/gogpt-interpreter/gogpt/entities/vars"
+
+	"github.com/alexgarzao/gogpt-interpreter/gogpt/adapters"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidStvInt(t *testing.T) {
+	// CP map:
+	//		0: (INT) 123
+	// VAR map:
+	//		0: (INT) Value
+	cp := constant_pool.NewCp()
+	cpIndex := cp.Add(123)
+	st := stack.NewStack()
+	stdout := adapters.NewFakeStdout()
+	vars := vars.NewVars()
+	varIndex := 0
+
+	// LDC 0
+	j := NewLdcOpcode()
+	j.CpIndex = cpIndex
+	// j.Execute(cp, vars, st, stdout)
+	j.Execute(cp, st, stdout)
+	stv, _ := st.Top()
+	assert.Equal(t, stv, stack.StackItem(123))
+
+	// STV 0
+	stvOpcode := NewStvOpcode()
+	stvOpcode.VarIndex = varIndex
+	stvOpcode.Execute(cp, vars, st, stdout)
+	vv, _ := vars.Get(varIndex)
+	assert.Equal(t, vv, constant_pool.CPItem(123))
+	assert.Equal(t, 0, st.Size())
+}
+
+func TestValidStvStr(t *testing.T) {
+	// CP map:
+	//		0: (STR) ABC
+	// VAR map:
+	//		0: (STR) Value
+	cp := constant_pool.NewCp()
+	cpIndex := cp.Add("ABC")
+	st := stack.NewStack()
+	stdout := adapters.NewFakeStdout()
+	vars := vars.NewVars()
+	varIndex := vars.Add()
+
+	// LDC 0
+	j := NewLdcOpcode()
+	j.CpIndex = cpIndex
+	// j.Execute(cp, vars, st, stdout)
+	j.Execute(cp, st, stdout)
+	stv, _ := st.Top()
+	assert.Equal(t, stv, stack.StackItem("ABC"))
+
+	// STV 0
+	stvOpcode := NewStvOpcode()
+	stvOpcode.VarIndex = varIndex
+	stvOpcode.Execute(cp, vars, st, stdout)
+	vv, _ := vars.Get(varIndex)
+	assert.Equal(t, vv, constant_pool.CPItem("ABC"))
+	assert.Equal(t, 0, st.Size())
+}
