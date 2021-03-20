397401415173107b6704c0b6013444f0093c18ed
Alex S. Garzão <alexgarzao@gmail.com>
2021-03-20 16:40:50 -0300

    chore: refactor in test

--- pkg/domain/usecases/instructions/ldci/ldc_test.go
+++ pkg/domain/usecases/instructions/ldci/ldc_test.go
@@ -35,25 +35,18 @@ func TestValidLdcInt123(t *testing.T) {
 }
 
 func TestValidLdcABC(t *testing.T) {
-	// CP map:
-	//		0: STR: "ABC"
+	// CP 0: STR: "ABC"
 	cp := cp.New()
 	cpIndex := cp.Add("ABC")
+
+	// LDC 0
 	vars := vars.New()
 	st := stack.New()
 	stdin := infrastructure.NewFakeStdin()
 	stdout := infrastructure.NewFakeStdout()
-	cpv, _ := cp.Get(0)
-	assert.Equal(t, "ABC", cpv)
-	stv, _ := st.Top()
-	assert.Equal(t, 0, stv)
-
-	// LDC 0
 	ldc := New()
 	ldc.CpIndex = cpIndex
 	ldc.Execute(cp, vars, st, stdin, stdout)
-	cpv, _ = cp.Get(0)
-	assert.Equal(t, "ABC", cpv)
-	stv, _ = st.Top()
+	stv, _ := st.Top()
 	assert.Equal(t, "ABC", stv)
 }
